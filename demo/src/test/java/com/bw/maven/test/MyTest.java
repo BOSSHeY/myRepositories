4fc7accc5730cf72642b42b02e1ecda72a3f92a7
heyaoyang <3346925220@qq.com>
2019-06-12 10:09:34 +0800

    new project

---
+++ demo/src/test/java/com/bw/maven/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.bw.maven.test;
+
+import org.junit.Test;
+
+/**
+ * 作者:何耀阳
+ * 时间:2019年6月8日
+ */
+public class MyTest {
+	@Test
+	public void test() {
+		System.out.println("HelloWorld!I'm Comming!");
+	}
+}
